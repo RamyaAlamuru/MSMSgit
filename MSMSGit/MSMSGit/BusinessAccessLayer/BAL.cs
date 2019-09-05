cdf59196930de06456c14df9f15b20e484c82210
RamyaAlamuru <ramya457.vtalent@gmail.com>
2019-09-05 15:02:44 +0530

    project created

---
+++ MSMSGit/MSMSGit/BusinessAccessLayer/BAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MSMSGit.Repository;
+using MSMSGit.Irepository;
+
+namespace MSMSGit.BusinessAccessLayer
+{
+    public class BAL
+    {
+        Ireposit obj1;
+        public BAL(Reposit obj2)
+        {
+            obj1 = obj2;
+        }
+    }
+}
---
+++ MSMSGit/MSMSGit/Repository/Reposit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MSMSGit.Irepository;
+
+namespace MSMSGit.Repository
+{
+    public class Reposit:Ireposit
+    {
+    }
+}
